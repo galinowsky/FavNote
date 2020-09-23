8bb0c1133f6067da03b336eb1f5f591062ba19f0
Jakub Galinski <thougless@gmail.com>
2020-09-23 11:35:08 +0200

    finally understand knobs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+addDecorator(withKnobs);
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,16 +3,16 @@ import React from 'react';
 import Button from 'components/Button/Button';
 import { withKnobs, select } from '@storybook/addon-knobs';
 
-const label = 'Colors';
-const options = {
-  Primary: 'hsl(49, 100%, 58%)',
-  Secondary: 'hsl(196, 83%, 75%)',
-  Tertiary: 'hsl(106, 47%, 64%)',
-};
-const defaultValue = 'hsl(196, 83%, 75%)';
-const groupId = 'GROUP-ID1';
+// const label = 'Colors';
+// const options = {
+//   Primary: 'hsl(49, 100%, 58%)',
+//   Secondary: 'hsl(196, 83%, 75%)',
+//   Tertiary: 'hsl(106, 47%, 64%)',
+// };
+// const defaultValue = 'hsl(49, 100%, 58%)';
+// // const groupId = 'GROUP-ID1';
 
-const value = select(label, options, defaultValue, groupId);
+// const value = select(label, options, defaultValue);
 
 export default {
   title: 'Example/Button', // podoajemy ścieżke
@@ -26,15 +26,33 @@ export default {
   },
 };
 
-const Template = args => <Button {...args}>{args.children}</Button>;
+const Template = args => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(49, 100%, 58%)',
+    Secondary: 'hsl(196, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const secondary = false;
+  const groupId = 'GROUP-ID2';
+  const value = select(label, options, defaultValue, secondary, groupId);
+
+  return (
+    <Button color={value} {...args}>
+      {' '}
+      Button
+    </Button>
+  );
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
-  color: value,
+  // color: 'Primary',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   secondary: true,
-  color: value,
+  // color: value,
 };
