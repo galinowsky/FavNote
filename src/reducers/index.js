4ba0f041283f178c9f74fc47caf5afda27ba20a5
Jakub Galinski <thougless@gmail.com>
2020-10-06 11:56:05 +0200

    all components rendering from store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,33 @@
 const initialState = {
   notes: [
     {
-      id: 1,
+      id:1,
       title: 'Wake me up when Vue ends',
       content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
       created: '1 day',
     },
+    {
+      id:2,
+      title: 'Como es An Gular?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '1 day',
+    },
+    {
+      id:3,
+      title: 'Du bist Reactish',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '5 days',
+    },
+    {
+      id:4,
+      title: 'Reactuj się kto moze!',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      created: '10 days',
+    }
   ],
   twitters: [
 
@@ -47,11 +68,32 @@ const initialState = {
       articleUrl: 'https://youtube.com/helloroman',
       created: '1 day',
     },
+    {
+      title: 'Wish you React',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    },
+    {
+      title: 'You gave React a bad name',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '5 days',
+    },
+    {
+      title: 'Is it React you looking for?',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '10 days',
+    },
   ],
 };
 
 const rootReducer = (state = initialState, action) => {
-return state 
+return state
 };
 
 export default rootReducer;
